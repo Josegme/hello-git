c8b4b08cfa489fb779dd6533f342da1415dd906d
Josegme <josegmescobar@gmail.com>
2024-08-08 14:54:55 -0300

    Guardando cambios en nueva rama

--- git_nuevarama.py
+++ git_nuevarama.py
@@ -1,8 +1,5 @@
 print('Esta es un fichero en una nueva rama')
-<<<<<<< HEAD
 print('Tengo que ver si esta Add y si esta Commiteado')
 
 print('Ahora voy a probar el STASH: guardado en mi local')
-=======
-print('Tengo que ver si esta Add y si esta Commiteado')
->>>>>>> nuevarama
+print('Ahora voy a probar si me deja moverme entre ramas sin hacer commits')
