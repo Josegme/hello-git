5c39256bd1e5c6a599e4522e941801f8c25d495e
Josegme <josegmescobar@gmail.com>
2024-08-08 13:06:11 -0300

    Resolviendo conflicto

--- hello_git3.py
+++ hello_git3.py
@@ -1,4 +1,2 @@
 print('Esta es el Hello git 3 - Luego de hacer un TAG')
 print('De esta manera con el TAG - Marco un cierto punto del trabajo y puedo regresar despues')
-
-print('Sigo trabajando en la rama Main - mientras el branch de la NuevaRama sigue su curso')
