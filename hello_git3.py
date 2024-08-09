70a87fe9c4aa7af296075c750824b2ed9c6f52cf
Josegme <josegmescobar@gmail.com>
2024-08-09 19:35:12 -0300

    nueva modificacion de 3

--- hello_git3.py
+++ hello_git3.py
@@ -1,2 +1,3 @@
 print('Esta es el Hello git 3 - Luego de hacer un TAG')
 print('De esta manera con el TAG - Marco un cierto punto del trabajo y puedo regresar despues')
+print('Este es un cambio para mostrar')
