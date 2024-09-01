582ac7ecd110d202b0ee03a73b7a386f96c54b03
josegme <josegmescobar@gmail.com>
2024-09-01 03:07:00 -0300

    añado una modificacion antes de prueba remota

--- hello_git.py
+++ hello_git.py
@@ -1,2 +1,14 @@
 print ('New Hello Git - probado otras modificaciones')
 print ('Estoy probando el Git Diff para ver cambios sin commitear')
+
+#26 Trabajando Local y Remoto
+#cuanto trabajamos remoto puede que hayamos hecho modificaciones sobre el mismo proyecto
+#Entonces tengo que sincronizar las modificaciones para que no exista conflicto
+#entonces vamos dejando todo el historial de trabajo con modificaciones pero en limipi
+
+#27 Como hacemos todo este proceso?
+#Autenticación SSH (es una forma segura de autenticación) Protocolo de Autenticación de Clave privada y Pública. 
+#es decir que GitHub va a saber que soy un usuario que puede acceder al material de trabajo.
+#Investigar mas este tema para saber como se utiliza 
+
+#29 Ahora vamos a ver si podemos trabajar con Git colocando en remoto en GitHub
